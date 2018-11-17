2572afc90b68fe2e36c52ff059b126555a21f0ee
kudotakuya <ttt.ttt.7733@gmail.com>
2018-11-17 16:47:20 +0900

    一時間ごとの平均値を算出

--- nayoro_watch WatchKit Extension/InterfaceController.swift
+++ nayoro_watch WatchKit Extension/InterfaceController.swift
@@ -27,28 +27,5 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-    
-    // HealthKitで扱うデータを管理するクラス(データの読み書きにはユーザの許可が必要)
-    let healthStore = HKHealthStore()
-    
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-        
-        // HealthKitがデバイス上で利用できるか確認
-        guard HKHealthStore.isHealthDataAvailable() else {
-            self.label.setText("not available")
-            return
-        }
-        
-        // アクセス許可をユーザに求める
-        let dataTypes = Set([HKQuantityType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)!])
-        self.healthStore.requestAuthorizationToShareTypes(nil, readTypes: dataTypes) { (success, error) -> Void in
-            guard success else {
-                self.label.setText("not allowed")
-                return
-            }
-        }
-    }
 
 }
--- nayoro_watch/ViewController.swift
+++ nayoro_watch/ViewController.swift
@@ -7,14 +7,110 @@
 //
 
 import UIKit
+import HealthKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    
+    private let healthStore = HKHealthStore()
+    // ワークアウトと心拍数を読み出しに設定
+    private let readDataTypes: Set<HKObjectType> = [
+        HKWorkoutType.workoutType(),
+        HKObjectType.quantityType(forIdentifier: .heartRate)!,
+        ]
+    
+    override func viewDidLoad() { do {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        healthStore.requestAuthorization(toShare: nil, read: readDataTypes) { (success, error) in
+            guard success, error == nil else {
+                return
+            }
+            self.getHeartRates()
+            self.getHeartRateWithFiveMinutes()
+        }
+    }}
+    
+    
+
+    private var statistics = [HKStatistics]()
+
+    private func getHeartRates() {
+        guard let type = HKObjectType.quantityType(forIdentifier: .heartRate) else {
+            return
+        }
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "yyyy/MM/dd"
+        let startDate = dateformatter.date(from: "2018/11/17")
+        let endDate = dateformatter.date(from: "2018/11/18")
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
+        let query = HKStatisticsQuery(quantityType: type, quantitySamplePredicate: predicate, options: [.discreteAverage, .discreteMin, .discreteMax]) { [unowned self] (query, statistic, error) in
+            guard let statistic = statistic, error == nil else {
+                return
+            }
+            self.statistics.append(statistic)
+            print("最低値 \(statistic.minimumQuantity()?.doubleValue(for: HKUnit(from: "count/min")) ?? 0) bpm")
+            print("最高値 \(statistic.maximumQuantity()?.doubleValue(for: HKUnit(from: "count/min")) ?? 0) bpm")
+            print("平均値 \(statistic.averageQuantity()?.doubleValue(for: HKUnit(from: "count/min")) ?? 0) bpm")
+        }
+        healthStore.execute(query)
     }
 
-
+    private var heartRateStatistics = [HKStatistics]()
+    
+    private func getHeartRateWithFiveMinutes() {
+        var dateComponents = DateComponents()
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "yyyy/MM/dd"
+        let startDate = dateformatter.date(from: "2018/11/17")
+        let endDate = dateformatter.date(from: "2018/11/18")
+        dateComponents.minute = 60  // 間隔時間
+        let quantityType = HKObjectType.quantityType(forIdentifier: .heartRate)!,
+        
+        query = HKStatisticsCollectionQuery(quantityType: quantityType, quantitySamplePredicate: nil, options: [.discreteAverage, .discreteMin, .discreteMax], anchorDate: startDate!, intervalComponents: dateComponents)
+        query.initialResultsHandler = { [unowned self] (query, result, error) in
+            guard let result = result, error == nil else {
+                return
+            }
+            result.enumerateStatistics(from: startDate!, to: endDate!) { (statistic, stop) in
+                self.heartRateStatistics.append(statistic)
+                print("平均値 \(statistic.averageQuantity()?.doubleValue(for: HKUnit(from: "count/min")) ?? 0) bpm")
+            }
+        }
+        healthStore.execute(query)
+    }
+    
+    
+    //    func getWorkout(){
+    //        // 取得する期間を設定
+    //        let dateformatter = DateFormatter()
+    //        dateformatter.dateFormat = "yyyy/MM/dd"
+    //        let startDate = dateformatter.date(from: "2017/12/01")
+    //        let endDate = dateformatter.date(from: "2018/01/01")
+    //
+    //        // 取得するデータを設定
+    //        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
+    //        let sort = [NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: true)]
+    //        let q = HKSampleQuery(sampleType: HKObjectType.workoutType(), predicate: predicate, limit: 0, sortDescriptors: sort, resultsHandler:{
+    //            (query, result, error) in
+    //
+    //            if let e = error {
+    //                print("Error: \(e.localizedDescription)")
+    //                return
+    //            }
+    //            DispatchQueue.main.async {
+    //                guard let r = result else {
+    //                    return
+    //                }
+    //
+    //                let workouts = r as! [HKWorkout]
+    //                for workout in workouts {
+    //                    print(workout.startDate)
+    //                    print(workout.totalDistance!)
+    //                    print(workout.totalEnergyBurned!)
+    //                }
+    //            }
+    //        })
+    //
+    //        healthStore.execute(q)
+    //    }
 }
-
